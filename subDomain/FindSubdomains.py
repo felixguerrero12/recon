4e8923ce1db891e1946f52c809dc57d369a8edbb
felixguerrero12 <felixguerrero12@gmail.com>
2018-10-23 00:30:25 -0400

    Edited the subdomain searcher script

--- subDomain/FindSubdomains.py
+++ subDomain/FindSubdomains.py
@@ -1,7 +1,7 @@
-import requests
+import requests, sys
 from bs4 import BeautifulSoup
 
-domain = raw_input("What domain do you want to search for? ")
+domain = sys.argv[1] 
 print "Downloading the results."
 data = requests.get('https://findsubdomains.com/subdomains-of/' + domain)
 
@@ -13,4 +13,4 @@ soup = BeautifulSoup(data.text, 'html.parser')
 data = []
 for tr in soup.find_all('a', { 'class': 'aggregated-link mobile-hidden' }):
     data.append(tr.attrs['href'])
-print data
+print str(data)
---
+++ subDomain/README.md
@@ -0,0 +1,1 @@
+This script uses findsubdomains.com to search for domain then parses the output into a JSON format.
