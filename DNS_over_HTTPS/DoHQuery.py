db4b59c16d77120469daeffe5f4ddefe49131b65
felixguerrero12 <felixguerrero12@gmail.com>
2018-10-26 00:17:26 -0400

    minor change in DoHQuery.py

--- DNS_over_HTTPS/DoHQuery.py
+++ DNS_over_HTTPS/DoHQuery.py
@@ -6,9 +6,9 @@ import random
 
 def query(domain, headers, dnstype):
     CFI = "https://cloudflare-dns.com/dns-query?name="+domain+"&type="+dnstype
-    CFII = "https://1.1.1.1/dns-query?name="+domain+"&type="+dnstype
+#CFII = "https://1.1.1.1/dns-query?name="+domain+"&type="+dnstype
     G = "https://dns.google.com/resolve?name="+domain+"&type="+dnstype
-    Q = "https://9.9.9.9/dns-query?name="+domain+"&type="dnstype
+    Q = "https://9.9.9.9/dns-query?name="+domain+"&type="+dnstype
     DoHServer = [CFI, CFII, G, Q]
     url = random.choice(DoHServer)
     r = requests.get(url, headers=headers, verify=True)
