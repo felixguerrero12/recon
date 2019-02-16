7b2ac192fb526542ac1618fde7e1320ac8e3ce33
felixguerrero12 <felixguerrero12@gmail.com>
2019-02-16 17:33:23 -0500

    Modification to the way is receives the output. Required a split.

--- subDomain/FindSubdomains.py
+++ subDomain/FindSubdomains.py
@@ -8,7 +8,7 @@ def getResults(domain):
     soup = BeautifulSoup(data.text, 'html.parser')
     data = []
     for tr in soup.find_all('a', {'class': 'aggregated-link mobile-hidden'}):
-        data.append(tr.attrs['href'])
+        data.append(tr.attrs['href'].split("/")[2])
     return data
 
 
