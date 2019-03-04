01ecaf8b276b2eb9f93d99d08c9d5e4a42359eb8
Felix Guerrero <felixguerrero12@users.noreply.github.com>
2019-03-04 08:42:10 -0500

    Update crtsh.py

--- subDomain/crtsh.py
+++ subDomain/crtsh.py
@@ -10,7 +10,8 @@ def getResults(domain):
 def main(argv):
     for i in range(1,len(sys.argv)):
 	domain = sys.argv[i]
-	print getResults(domain)
+	output = getResults(domain)
+	print '\n'.join(output)
 
 
 if __name__ == "__main__":
